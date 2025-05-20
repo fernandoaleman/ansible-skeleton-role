bfe7e212f0c4153c89cf5794e9302bf535c50af6
Fernando Alemán <fernandoaleman@mac.com>
2025-05-20 13:48:30 -0500

    Simplify molecule testing

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,13 +1,8 @@
 # molecule/default/tests/test_default.py
-from utils import load_role_vars
-
-vars = load_role_vars()
-
-etc_dir = vars["etc_dir"]
 
 
 def test_host_is_reachable(host):
     """Ensure etc directory exists"""
-    etc = host.file(etc_dir)
+    etc = host.file("/etc")
     assert etc.exists
     assert etc.is_directory
